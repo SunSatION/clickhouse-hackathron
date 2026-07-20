637702c7171eb4bff795d747e98a25cccd5cbebe
Dorian Bugeja <sunsation.xsoft@gmail.com>
2026-07-20 22:28:00 +0200

    test: ESM default export .mjs

--- api/health.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  try {
-    res.status(200).json({ ok: true, method: req.method });
-  } catch (e) {
-    res.status(500).end("err: " + e.message);
-  }
-};
