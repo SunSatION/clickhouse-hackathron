8880d49067a418a968de63ed7d39381446097f50
Dorian Bugeja <sunsation.xsoft@gmail.com>
2026-07-20 16:45:19 +0200

    test: minimal handler to isolate Vercel invocation failure

--- api/map/airports.js
+++ api/map/airports.js
@@ -1,12 +1,3 @@
-import data from "../../public/data/airports.json";
-
-export default function handler(req, res) {
-  res.setHeader("Cache-Control", "public, max-age=300, s-maxage=3600");
-  const airline = (req.query?.airline || "Ryanair").toString();
-  const airports = (data.airports || []).map((a) => ({
-    ...a,
-    originCount: 0,
-    destinationCount: 0,
-  }));
-  res.json({ ok: true, airline, count: airports.length, airports });
-}
+module.exports = (req, res) => {
+  res.json({ ok: true, message: "hello from airports" });
+};
